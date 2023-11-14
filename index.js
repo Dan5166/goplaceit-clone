d93626068c7d8147f681c563381cb5d78bb0c99d
danyael_vasquez <d.vsquezojeda@uandresbello.edu>
2023-11-14 20:17:23 -0300

    testing-responsive 1

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('button-show-list').addEventListener('click', function() {
+    var propertyList = document.getElementById('property-list');
+    propertyList.classList.toggle('active');
+  });
